e83822526fb8430bac47e1ae35cdc1bf900a3495
Alrandis <79513543+Alrandis@users.noreply.github.com>
2022-06-15 17:46:09 +0700

    Слил все комнаты в одну сцену

--- Assets/Scripts/SceneSwitch.cs
+++ Assets/Scripts/SceneSwitch.cs
@@ -5,12 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class SceneSwitch : MonoBehaviour
 {
-    [SerializeField] private int sceneNumber;
+    
     [SerializeField] private Vector2 playerPosition;
 
     private void OnMouseDown()
-    {
-        SceneManager.LoadScene(sceneNumber);
+    { 
         FindObjectOfType<PlayerController>().PutInPoint(playerPosition);
     }
 }
